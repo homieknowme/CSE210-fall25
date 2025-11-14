67cd9cbc15f510226e7bb49efc942092b7916750
homieknowme <homieknowme@gmail.com>
2025-11-14 04:57:00 -0600

    W09 Prepare: Learning Activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square("Red", 5));
+        shapes.Add(new Rectangle("Cyan", 3, 9));
+        shapes.Add(new Circle("Magenta", 2));
+        shapes.Add(new Square("Blue", 8));
+        shapes.Add(new Rectangle("Beige", 2, 4));
+        shapes.Add(new Circle("Pink", 7));
+        shapes.Add(new Square("Yellow", 15));
+        shapes.Add(new Rectangle("Green", 9, 12));
+        shapes.Add(new Circle("Purple", 21));
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}");
+            Console.WriteLine($"Area: {shape.GetArea()}");
+            Console.WriteLine();
+        }
+        
     }
+
 }
